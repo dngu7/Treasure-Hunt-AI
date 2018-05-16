0cb1f67c9a1b35d01d07902d2836c036b829543f
dngu7 <david.nguyen@protonmail.com>
2018-05-16 22:04:09 +1000

    deleted searchmap

--- agent/searchMap.py
+++
@@ -1,9 +0,0 @@
-
-
-def keyboardOutput(agentDirection, newDirection, movement):
-    agentDirectionList = [0,90,180,270]
-    newDirectionList = ['U','D','R','L']
-
-    if (agentDirection in agentDirectionList) and (newDirection in newDirectionList) and movement >=0:
-        
-
